d65ffe90c4fab2d9f7d4ddde38c5db76363432ef
Josephine Feely <jojofeely@yahoo.co.uk>
2019-03-20 22:58:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// add solution here
+function theBeatlesPlay(musician, instrument){
+  var array = []
+  for (var i = 0; i < musician.length; i++){
+    array.push(musician[i] + ' plays ' + instrument[i]);
+  }
+  return array
+}
